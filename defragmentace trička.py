d1a6de079b07572ca83c643994f057f838784ec3
Daniel Perout <daniel.perout@brnoc.cz>
2021-07-14 21:38:06 +0200

    Už by to chtělo vyřešit soubory

--- defragmentace trička.py
+++ defragmentace trička.py
@@ -4,39 +4,52 @@
 #zelená		-- .d
 #margin		–– ?
 
-sirkaObd	= 13
-sirkaWin	= 800
-vyskaWin	= 500
-margin		= 3 #BÚNO
-sirkaMar	= sirkaObd + margin
-sance 		= 0
-sancePlus	= 0.0003
+sirkaObd			= 13
+margin				= 3
+pricitam			= 0.1
+sirkaMar			= sirkaObd + margin
+sance 				= 0
+sancePlus			= 0.0003
+sirkaVelkyObdelnik	= (sirkaObd+margin)*12
+sirkaWin			= sirkaVelkyObdelnik*4
+vyskaWin			= 500
 
 import random,sys #<-- takto?, Tom řekl ok
 
 def obdelnicek(x,y,barva, bůl):
 	if bůl:
-		mamToPosunoutInt = -3
+		posunSirka = margin + pricitam
+		posunXAxis = -margin
 	else:
-		mamToPosunoutInt = 0
-	return f"<rect class=\"{barva}\" width=\"{sirkaObd - mamToPosunoutInt}\" height=\"{sirkaObd}\" x=\"{x*sirkaMar + mamToPosunoutInt}\" y=\"{y*sirkaMar}\" z=\"100\"/>"
+		posunSirka = pricitam
+		posunXAxis = 0
+	return f"<rect class=\"{barva}\" width=\"{sirkaObd + posunSirka}\" height=\"{sirkaObd}\" x=\"{x*sirkaMar + posunXAxis}\" y=\"{y*sirkaMar}\" z=\"100\"/>"
 
-with open("defragmentace trička.svg") as soubor:
-	nactenySoubor = soubor.read()
-	#print(nactenySoubor[0:-6])
-	#nactenySoubor = nactenySoubor[0:-6]
-	posledniPosledniBarva = ""
-	for yy in range(int(vyskaWin / sirkaMar)):
-		posledniBarva = ""
-		for xx in range(int(sirkaWin / sirkaMar)):
-			sance += sancePlus
-			#if(random.random()>sance):
-			novaBarva = random.choice(["a", "b", "c", "d"])
-			#else:
-			#	novaBarva = posledniPosledniBarva
-			sys.stderr.write(f"{novaBarva} -lol- {posledniBarva} \n")
-			nactenySoubor += obdelnicek(xx, yy, novaBarva, novaBarva == posledniBarva)
-			posledniBarva = novaBarva
-			posledniPosledniBarva = posledniBarva
-	nactenySoubor += '<rect class="a" width="200" height="80" x="0" y="414" z="420"/><rect class="b" x="200" y="414" width="200" height="80" z="420"/><rect class="c" x="400" y="414" width="200" height="80" z="420"/><rect class="d" x="600" y="414" width="200" height="80" z="420"/></svg>'
-	print(nactenySoubor)
+nactenySoubor = f'''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 {sirkaWin} {vyskaWin}">
+	<defs>
+		<style>.a{{fill:#339cd6;}}.b{{fill:#eedc64;}}.c{{fill:#bd5349;}}.d{{fill:#72a54f;}}</style>
+	</defs>
+	<title>defragmentace trička</title>
+	<rect class="a" width="{sirkaWin}" height="494" x="0" y="0"/>'''
+#print(nactenySoubor[0:-6])
+#nactenySoubor = nactenySoubor[0:-6]
+posledniPosledniBarva = ""
+for yy in range(int(vyskaWin / sirkaMar)):
+	posledniBarva = ""
+	for xx in range(int(sirkaWin / sirkaMar)):
+		sance += sancePlus
+		#if(random.random()>sance):
+		novaBarva = random.choice(["a", "b", "c", "d"])
+		#else:
+		#	novaBarva = posledniPosledniBarva
+		#sys.stderr.write(f"{novaBarva} -lol- {posledniBarva} \n")
+		nactenySoubor += obdelnicek(xx, yy, novaBarva, novaBarva == posledniBarva)
+		posledniBarva = novaBarva
+		posledniPosledniBarva = posledniBarva
+		nactenySoubor += f'<rect class="a" width="{sirkaVelkyObdelnik - margin*0.5}" height="80" x="0" y="414"/>'
+		nactenySoubor += f'<rect class="b" width="{sirkaVelkyObdelnik}" height="80" x="{1*sirkaVelkyObdelnik - margin*0.5}" y="414"/>'
+		nactenySoubor += f'<rect class="c" width="{sirkaVelkyObdelnik}" height="80" x="{2*sirkaVelkyObdelnik - margin*0.5}" y="414"/>'
+		nactenySoubor += f'<rect class="d" width="{sirkaVelkyObdelnik + margin*0.5}" height="80" x="{3*sirkaVelkyObdelnik - margin*0.5}" y="414"/>'
+#nactenySoubor += f'<rect class="a" width="{sirkaVelkyObdelnik}" height="80" x="0" y="414" z="420"/><rect class="b" x="200" y="414" width="{sirkaVelkyObdelnik}" height="80" z="420"/><rect class="c" x="400" y="414" width="{sirkaVelkyObdelnik}" height="80" z="420"/><rect class="d" x="600" y="414" width="{sirkaVelkyObdelnik}" height="80" z="420"/></svg>'
+nactenySoubor += "</svg>"
+print(nactenySoubor)
